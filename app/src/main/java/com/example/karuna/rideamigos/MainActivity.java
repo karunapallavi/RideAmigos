adf79a94ba4f2ff16a1c4707eb5140c56f0394ea
karunapallavi <karuna.pallavi@gmail.com>
2017-09-05 15:45:13 -0500

    Sending location to a url

---
+++ app/src/main/java/com/example/karuna/rideamigos/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.karuna.rideamigos;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    String la = "", lo = "";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLocation = (Button) findViewById(R.id.button);
+        Button btnStop = (Button) findViewById(R.id.button2);
+        //Button btnStart = (Button)findViewById(R.id.button);
+        final CountDownTimer cdtimer = new CountDownTimer(15000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                postData(la, lo);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+
+        btnLocation.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Acquire a reference to the system Location Manager
+                LocationManager locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
+                // Define a listener that responds to location updates
+                LocationListener locationListener = new LocationListener() {
+                    public void onLocationChanged(Location location) {
+                        // Called when a new location is found by the network location provider.
+                        la = Double.toString(location.getLatitude());
+                        lo = Double.toString(location.getLongitude());
+                        TextView tv = (TextView) findViewById(R.id.textView);
+                        tv.setText("Your Location is:" + la + "--" + lo);
+                    }
+
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+
+                    public void onProviderEnabled(String provider) {
+                    }
+
+                    public void onProviderDisabled(String provider) {
+                    }
+                };
+                // Register the listener with the Location Manager to receive location updates
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            }
+        });
+
+        Button btnStart = (Button) findViewById(R.id.button);
+        btnStart.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                postData(la, lo);
+                cdtimer.start();
+            }
+
+
+
+        });
+
+        btnStop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cdtimer.cancel();
+            }
+        });
+
+    }
+
+
+    public void postData(String la, String lo) {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet htget = new HttpGet("<add url here>" + la + "/" + lo);
+
+        try {
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(htget);
+            String resp = response.getStatusLine().toString();
+            Toast.makeText(this, resp, 5000).show();
+
+
+        } catch (ClientProtocolException e) {
+            Toast.makeText(this, "Error", 5000).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error", 5000).show();
+        }
+    }
+
+    
+}
